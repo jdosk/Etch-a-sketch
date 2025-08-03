ed0369187bda708b380422343283803f33617fdc
jdosk <jdoskocil86@gmail.com>
2025-08-03 14:34:46 -0700

    Added for loop to dynamically create the squares

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const container = document.querySelector(".container");
 
+function randomColor() {
+    
+}
+
+
+// add the internal boxes
+for (let i = 0; i < 16; i++) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('hover-box');
+  newDiv.style.height = '100px';
+  newDiv.style.width = '100px';
+
+  newDiv.addEventListener('mouseover', () => {
+    newDiv.style.backgroundColor = 'blue';
+  });
+
+  container.appendChild(newDiv);
+}
