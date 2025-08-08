c3b31bf7bc184f26136691dd78b0b32149f853ef
jdosk <jdoskocil86@gmail.com>
2025-08-08 07:35:03 -0700

    Added button functionality to create the grid.

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 const container = document.querySelector(".container");
+//const resetButton = document.querySelector(".reset").addEventListener(
 
-function randomColor() {
-    
-}
-
-
-// add the internal boxes
-for (let i = 0; i < 16; i++) {
-  const newDiv = document.createElement('div');
-  newDiv.classList.add('hover-box');
-  newDiv.style.height = '100px';
-  newDiv.style.width = '100px';
-
-  newDiv.addEventListener('mouseover', () => {
-    newDiv.style.backgroundColor = 'blue';
+//)
+document.querySelector(".reset").addEventListener("click", function () {
+    const userInput = prompt("Enter a number:");
+    const num = parseFloat(userInput);
+    if (!isNaN(num) && num > 0 && num < 101) {
+      setGrid(num);
+    } else {
+      alert("That's not a valid number. Please enter a number between 1 and 100.");
+    }
   });
 
-  container.appendChild(newDiv);
+function randomColor() {
+
 }
+
+function setGrid(num) {
+  container.innerHTML = "";
+
+  for (let i = 0; i < num * num; i++) {
+    const newDiv = document.createElement('div');
+
+    newDiv.classList.add('hover-box');
+    newDiv.style.height = 800 / num + "px";
+    newDiv.style.width = 800 / num + "px";
+
+    newDiv.addEventListener('mouseover', () => {
+      newDiv.style.backgroundColor = 'orange';
+    });
+
+    container.appendChild(newDiv);
+  }
+}
+
+// add the internal boxes
+//for (let i = 0; i < 256; i++) {
+//  const newDiv = document.createElement('div');
+//  newDiv.classList.add('hover-box');
+//  newDiv.style.height = '50px';
+//  newDiv.style.width = '50px';
+//
+//  newDiv.addEventListener('mouseover', () => {
+//    newDiv.style.backgroundColor = 'orange';
+//  });
+//
+//  container.appendChild(newDiv);
+//}
