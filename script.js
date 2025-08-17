d93395bd96a4d53ea59f6d5d06bb837d59b9207d
jdosk <jdoskocil86@gmail.com>
2025-08-17 12:16:50 -0700

    Finalized color randomization and opacity changes, added comments and cleaned up code

--- script.js
+++ script.js
@@ -12,18 +12,32 @@ document.querySelector(".reset").addEventListener("click", function () {
     } else {
       alert("That's not a valid number. Please enter a number between 1 and 100.");
     }
+});
+
+// Function to set the hover effect for the hover boxes
+function setHoverColors(div) {
+  // Store opacity in a dataset
+  div.dataset.opacity = 1;
+
+  div.addEventListener('mouseover', () => {
+    let opacity = Math.min(1, parseFloat(div.dataset.opacity) - 0.1);
+    div.dataset.opacity = opacity;
+    // Randomize colors
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    // apply random colors and next opacity level
+    div.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${1 - opacity})`;
   });
-
-// Function to randomize the hover color
-function randomColor() {
-
 }
 
 // Removes previous grid and sets the new grid with number of sides based on user input
 function setGrid(num) {
+  // Reset the grid, and set hover box size
   container.innerHTML = "";
   let size = 800 / num;
 
+  // Loop to create the rows which hold the hover boxes
   for (let i = 0; i < num; i++) {
 
     const newRow = document.createElement('div');
@@ -32,6 +46,7 @@ function setGrid(num) {
     
     container.appendChild(newRow);
 
+    // Loop through each row and create the hover boxes with necessary attributes
     for (let j = 0; j < num; j++) {
 
       const newDiv = document.createElement('div');
@@ -40,9 +55,8 @@ function setGrid(num) {
       newDiv.style.height = size + "px";
       newDiv.style.width = size + "px";
 
-      newDiv.addEventListener('mouseover', () => {
-        newDiv.style.backgroundColor = 'blue';
-      });
+      // Initialize event handlers
+      setHoverColors(newDiv);
 
       newRow.appendChild(newDiv);
     }
